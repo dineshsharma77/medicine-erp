7def421d38a6d7ecc59c4cda974dcf9e3f437876
Harpreet Singh <thesinghharpreet@gmail.com>
2021-06-18 16:16:20 +0530

    Added index

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('/', views.index, name='index'),
     path('contact', views.contact, name='contact'),
     path('contact/save', views.save_contact, name='save_contact'),
 
